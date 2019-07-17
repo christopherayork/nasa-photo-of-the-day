f3e9eb7531626b6037131121fff94da8ca29b66f
Chris York <christopherayork1992@gmail.com>
2019-07-17 15:33:20 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const axios = require('axios');
+
+let defAPI = 'https://api.nasa.gov/planetary/apod?api_key=dpGik4kM4nP7gKf26s0SqiFdXViLCDBIewiNhBSN';
+
 function App() {
+
+  let [API, setAPI] = useState(defAPI);
+  let [content, setContent] = useState('');
+  useEffect(() => {
+    axios.get(API)
+        .then(res => {
+          setContent(res);
+          console.log(res);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+  }, [API]);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      {/* We need a component for the header and post, with video title, video subcomponent, and explanation */}
     </div>
   );
 }
